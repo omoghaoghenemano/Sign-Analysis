38037c29fe85038aece3ee12bfb96c0a59ea994c
Oghenemano Utomudo Omogha <manoomogha@yahoo.com>
2024-06-12 13:05:03 +0200

    fix: TransferRelation.evaluate(ADD, ⊥, {0,–}) should return ⊥

--- src/de/uni_passau/fim/se2/sa/sign/interpretation/SignTransferRelation.java
+++ src/de/uni_passau/fim/se2/sa/sign/interpretation/SignTransferRelation.java
@@ -54,6 +54,8 @@ public class SignTransferRelation implements TransferRelation {
         if (pLHS == SignValue.BOTTOM && pRHS == SignValue.MINUS) return SignValue.BOTTOM;
         if (pLHS == SignValue.PLUS && pRHS == SignValue.PLUS) return SignValue.PLUS;
         if (pLHS == SignValue.MINUS && pRHS == SignValue.MINUS) return SignValue.MINUS;
+        if (pLHS == SignValue.PLUS_MINUS && pRHS == SignValue.PLUS) return SignValue.PLUS;
+        if (pLHS == SignValue.BOTTOM && pRHS == SignValue.ZERO_PLUS) return SignValue.BOTTOM;
         if ((pLHS == SignValue.PLUS && pRHS == SignValue.MINUS) || (pLHS == SignValue.MINUS && pRHS == SignValue.PLUS)) return SignValue.TOP;
         return SignValue.TOP;
 
