d88e600873de39489505554c148d3140fc78e837
Oghenemano Utomudo Omogha <manoomogha@yahoo.com>
2024-06-13 10:06:30 +0200

    add test for sign transfer relation

--- test/de/uni_passau/fim/se2/sa/sign/interpretation/SignInterpreterTest.java
+++ test/de/uni_passau/fim/se2/sa/sign/interpretation/SignInterpreterTest.java
@@ -4,20 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.InsnNode;
 import org.objectweb.asm.tree.LdcInsnNode;
 import org.objectweb.asm.tree.analysis.AnalyzerException;
-
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 public class SignInterpreterTest {
-
-    @Test
-    public void testConstructorWithInvalidAPI() {
-        assertDoesNotThrow( () -> new SignInterpreter(Opcodes.ASM4));
-    }
 
     @Test
     public void testNewValue() {
@@ -27,7 +21,120 @@ public class SignInterpreterTest {
         assertEquals(SignValue.TOP, interpreter.newValue(Type.INT_TYPE));
     }
 
-    
+    @Test
+    public void testNewOperation() {
+        SignInterpreter interpreter = new SignInterpreter();
+
+        AbstractInsnNode instr = new InsnNode(Opcodes.ICONST_0);
+        try {
+            assertEquals(SignValue.ZERO, interpreter.newOperation(instr));
+        } catch (AnalyzerException e) {
+            fail("AnalyzerException should not be thrown");
+        }
+
+        instr = new InsnNode(Opcodes.ICONST_1);
+        try {
+            assertEquals(SignValue.PLUS, interpreter.newOperation(instr));
+        } catch (AnalyzerException e) {
+            fail("AnalyzerException should not be thrown");
+        }
+
+        instr = new InsnNode(Opcodes.ICONST_M1);
+        try {
+            assertEquals(SignValue.MINUS, interpreter.newOperation(instr));
+        } catch (AnalyzerException e) {
+            fail("AnalyzerException should not be thrown");
+        }
+
+        instr = new InsnNode(Opcodes.BIPUSH);
+        try {
+            assertEquals(SignValue.TOP, interpreter.newOperation(instr));
+        } catch (AnalyzerException e) {
+            fail("AnalyzerException should not be thrown");
+        }
+
+        instr = new InsnNode(Opcodes.SIPUSH);
+        try {
+            assertEquals(SignValue.TOP, interpreter.newOperation(instr));
+        } catch (AnalyzerException e) {
+            fail("AnalyzerException should not be thrown");
+        }
+
+        LdcInsnNode ldcInsnNode = new LdcInsnNode(0);
+        try {
+            assertEquals(SignValue.ZERO, interpreter.newOperation(ldcInsnNode));
+        } catch (AnalyzerException e) {
+            fail("AnalyzerException should not be thrown");
+        }
+
+        ldcInsnNode = new LdcInsnNode(1);
+        try {
+            assertEquals(SignValue.PLUS, interpreter.newOperation(ldcInsnNode));
+        } catch (AnalyzerException e) {
+            fail("AnalyzerException should not be thrown");
+        }
+
+    }
+
+    @Test
+    public void testCopyOperation() {
+        SignInterpreter interpreter = new SignInterpreter();
+        AbstractInsnNode instr = new InsnNode(Opcodes.NOP);
+        SignValue value = SignValue.TOP;
+        assertEquals(value, interpreter.copyOperation(instr, value));
+    }
+
+    @Test
+    public void testUnaryOperation() {
+        SignInterpreter interpreter = new SignInterpreter();
+
+        AbstractInsnNode instr = new InsnNode(Opcodes.INEG);
+        try {
+            assertEquals(SignValue.MINUS, interpreter.unaryOperation(instr, SignValue.PLUS));
+            assertEquals(SignValue.PLUS, interpreter.unaryOperation(instr, SignValue.MINUS));
+            assertEquals(SignValue.ZERO, interpreter.unaryOperation(instr, SignValue.ZERO));
+        } catch (AnalyzerException e) {
+            fail("AnalyzerException should not be thrown");
+        }
+
+        instr = new InsnNode(Opcodes.IINC);
+        try {
+            assertEquals(SignValue.TOP, interpreter.unaryOperation(instr, SignValue.PLUS));
+        } catch (AnalyzerException e) {
+            fail("AnalyzerException should not be thrown");
+        }
+
+
+    }
+
+    @Test
+    public void testBinaryOperation() {
+        SignInterpreter interpreter = new SignInterpreter();
+        AbstractInsnNode instr = new InsnNode(Opcodes.IADD);
+        assertEquals(SignValue.TOP, interpreter.binaryOperation(instr, SignValue.PLUS, SignValue.MINUS));
+    }
+
+    @Test
+    public void testTernaryOperation() {
+        SignInterpreter interpreter = new SignInterpreter();
+        AbstractInsnNode instr = new InsnNode(Opcodes.NOP);
+        assertNull(interpreter.ternaryOperation(instr, SignValue.PLUS, SignValue.MINUS, SignValue.ZERO));
+    }
+
+    @Test
+    public void testNaryOperation() {
+        SignInterpreter interpreter = new SignInterpreter();
+        AbstractInsnNode instr = new InsnNode(Opcodes.NOP);
+        assertEquals(SignValue.TOP, interpreter.naryOperation(instr, List.of(SignValue.PLUS, SignValue.MINUS)));
+    }
+
+    @Test
+    public void testReturnOperation() {
+        SignInterpreter interpreter = new SignInterpreter();
+        AbstractInsnNode instr = new InsnNode(Opcodes.RETURN);
+        assertDoesNotThrow(() -> interpreter.returnOperation(instr, SignValue.PLUS, SignValue.MINUS));
+    }
+
     @Test
     public void testMerge() {
         SignInterpreter interpreter = new SignInterpreter();
---
+++ test/de/uni_passau/fim/se2/sa/sign/interpretation/SignTransferRelationTest.java
@@ -0,0 +1,91 @@
+package de.uni_passau.fim.se2.sa.sign.interpretation;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SignTransferRelationTest {
+
+    @Test
+    public void testEvaluateInt() {
+        SignTransferRelation transferRelation = new SignTransferRelation();
+
+        assertEquals(SignValue.MINUS, transferRelation.evaluate(-10));
+        assertEquals(SignValue.ZERO, transferRelation.evaluate(0));
+        assertEquals(SignValue.PLUS, transferRelation.evaluate(10));
+    }
+
+    @Test
+    public void testEvaluateUnaryOperation() {
+        SignTransferRelation transferRelation = new SignTransferRelation();
+
+        assertEquals(SignValue.MINUS, transferRelation.evaluate(TransferRelation.Operation.NEG, SignValue.PLUS));
+        assertEquals(SignValue.PLUS, transferRelation.evaluate(TransferRelation.Operation.NEG, SignValue.MINUS));
+        assertEquals(SignValue.ZERO, transferRelation.evaluate(TransferRelation.Operation.NEG, SignValue.ZERO));
+        assertEquals(SignValue.ZERO_MINUS, transferRelation.evaluate(TransferRelation.Operation.NEG, SignValue.ZERO_PLUS));
+        assertEquals(SignValue.ZERO_PLUS, transferRelation.evaluate(TransferRelation.Operation.NEG, SignValue.ZERO_MINUS));
+        assertEquals(SignValue.PLUS_MINUS, transferRelation.evaluate(TransferRelation.Operation.NEG, SignValue.PLUS_MINUS));
+        assertEquals(SignValue.TOP, transferRelation.evaluate(TransferRelation.Operation.NEG, SignValue.TOP));
+        assertEquals(SignValue.BOTTOM, transferRelation.evaluate(TransferRelation.Operation.NEG, SignValue.BOTTOM));
+        assertEquals(SignValue.UNINITIALIZED_VALUE, transferRelation.evaluate(TransferRelation.Operation.NEG, SignValue.UNINITIALIZED_VALUE));
+    }
+
+    @Test
+    public void testEvaluateBinaryOperationAdd() {
+        SignTransferRelation transferRelation = new SignTransferRelation();
+
+        assertEquals(SignValue.BOTTOM, transferRelation.evaluate(TransferRelation.Operation.ADD, SignValue.BOTTOM, SignValue.PLUS));
+        assertEquals(SignValue.ZERO, transferRelation.evaluate(TransferRelation.Operation.ADD, SignValue.ZERO, SignValue.ZERO));
+        assertEquals(SignValue.PLUS, transferRelation.evaluate(TransferRelation.Operation.ADD, SignValue.PLUS, SignValue.PLUS));
+        assertEquals(SignValue.MINUS, transferRelation.evaluate(TransferRelation.Operation.ADD, SignValue.MINUS, SignValue.MINUS));
+        assertEquals(SignValue.TOP, transferRelation.evaluate(TransferRelation.Operation.ADD, SignValue.PLUS, SignValue.MINUS));
+        assertEquals(SignValue.TOP, transferRelation.evaluate(TransferRelation.Operation.ADD, SignValue.PLUS_MINUS, SignValue.MINUS));
+        assertEquals(SignValue.ZERO_PLUS, transferRelation.evaluate(TransferRelation.Operation.ADD, SignValue.ZERO_PLUS, SignValue.ZERO_PLUS));
+        assertEquals(SignValue.ZERO_MINUS, transferRelation.evaluate(TransferRelation.Operation.ADD, SignValue.ZERO_MINUS, SignValue.ZERO_MINUS));
+    }
+
+    @Test
+    public void testEvaluateBinaryOperationSub() {
+        SignTransferRelation transferRelation = new SignTransferRelation();
+
+        assertEquals(SignValue.BOTTOM, transferRelation.evaluate(TransferRelation.Operation.SUB, SignValue.BOTTOM, SignValue.PLUS));
+        assertEquals(SignValue.PLUS_MINUS, transferRelation.evaluate(TransferRelation.Operation.SUB, SignValue.PLUS_MINUS, SignValue.ZERO));
+        assertEquals(SignValue.MINUS, transferRelation.evaluate(TransferRelation.Operation.SUB, SignValue.ZERO, SignValue.PLUS));
+        assertEquals(SignValue.PLUS, transferRelation.evaluate(TransferRelation.Operation.SUB, SignValue.ZERO, SignValue.MINUS));
+        assertEquals(SignValue.ZERO_MINUS, transferRelation.evaluate(TransferRelation.Operation.SUB, SignValue.ZERO, SignValue.ZERO_PLUS));
+        assertEquals(SignValue.ZERO_PLUS, transferRelation.evaluate(TransferRelation.Operation.SUB, SignValue.ZERO, SignValue.ZERO_MINUS));
+        assertEquals(SignValue.PLUS, transferRelation.evaluate(TransferRelation.Operation.SUB, SignValue.PLUS, SignValue.ZERO));
+        assertEquals(SignValue.PLUS, transferRelation.evaluate(TransferRelation.Operation.SUB, SignValue.PLUS, SignValue.MINUS));
+        assertEquals(SignValue.TOP, transferRelation.evaluate(TransferRelation.Operation.SUB, SignValue.MINUS, SignValue.MINUS));
+    }
+
+    @Test
+    public void testEvaluateBinaryOperationMul() {
+        SignTransferRelation transferRelation = new SignTransferRelation();
+
+        assertEquals(SignValue.BOTTOM, transferRelation.evaluate(TransferRelation.Operation.MUL, SignValue.BOTTOM, SignValue.PLUS));
+        assertEquals(SignValue.ZERO, transferRelation.evaluate(TransferRelation.Operation.MUL, SignValue.ZERO, SignValue.PLUS));
+        assertEquals(SignValue.PLUS_MINUS, transferRelation.evaluate(TransferRelation.Operation.MUL, SignValue.PLUS_MINUS, SignValue.PLUS));
+        assertEquals(SignValue.MINUS, transferRelation.evaluate(TransferRelation.Operation.MUL, SignValue.MINUS, SignValue.PLUS));
+        assertEquals(SignValue.PLUS, transferRelation.evaluate(TransferRelation.Operation.MUL, SignValue.MINUS, SignValue.MINUS));
+        assertEquals(SignValue.ZERO_PLUS, transferRelation.evaluate(TransferRelation.Operation.MUL, SignValue.ZERO_PLUS, SignValue.PLUS));
+        assertEquals(SignValue.ZERO_MINUS, transferRelation.evaluate(TransferRelation.Operation.MUL, SignValue.ZERO_PLUS, SignValue.MINUS));
+        assertEquals(SignValue.ZERO, transferRelation.evaluate(TransferRelation.Operation.MUL, SignValue.ZERO, SignValue.ZERO));
+    }
+
+    @Test
+    public void testEvaluateBinaryOperationDiv() {
+        SignTransferRelation transferRelation = new SignTransferRelation();
+
+        assertEquals(SignValue.BOTTOM, transferRelation.evaluate(TransferRelation.Operation.DIV, SignValue.BOTTOM, SignValue.PLUS));
+        assertEquals(SignValue.ZERO, transferRelation.evaluate(TransferRelation.Operation.DIV, SignValue.ZERO, SignValue.PLUS));
+        assertEquals(SignValue.ZERO_MINUS, transferRelation.evaluate(TransferRelation.Operation.DIV, SignValue.MINUS, SignValue.PLUS));
+        assertEquals(SignValue.ZERO_PLUS, transferRelation.evaluate(TransferRelation.Operation.DIV, SignValue.MINUS, SignValue.MINUS));
+        assertEquals(SignValue.BOTTOM, transferRelation.evaluate(TransferRelation.Operation.DIV, SignValue.PLUS, SignValue.ZERO));
+        assertEquals(SignValue.TOP, transferRelation.evaluate(TransferRelation.Operation.DIV, SignValue.PLUS_MINUS, SignValue.MINUS));
+        assertEquals(SignValue.BOTTOM, transferRelation.evaluate(TransferRelation.Operation.DIV, SignValue.ZERO, SignValue.ZERO));
+    }
+}
+
+
+
+
